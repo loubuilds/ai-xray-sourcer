fe2808f42092f14a6a7ff2ef73d86036a8061634
loubuilds <louise_whitehead@hotmail.com>
2026-02-07 19:41:35 +0000

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>AI X-ray Sourcer</h1>
+      <p>It works 🎉</p>
+    </main>
+  );
+}
