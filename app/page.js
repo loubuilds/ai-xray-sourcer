48ccaf6d07cf32521893661665af27224d4d5a54
loubuilds <louise_whitehead@hotmail.com>
2026-02-07 19:43:46 +0000

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>AI X-ray Sourcer</h1>
-      <p>It works 🎉</p>
-    </main>
-  );
-}
